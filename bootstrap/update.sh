6e371927268b6a1bbc14bc569d71da92250a86ae
Vasil Zlatanov <v@skozl.com>
2020-12-16 22:06:33 +0000

    Add image-automation-controller to update

--- bootstrap/update.sh
+++ bootstrap/update.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-flux install --components=source-controller,kustomize-controller,helm-controller,notification-controller \
+flux install \
+  --components=source-controller,kustomize-controller,helm-controller,notification-controller \
+  --components-extra=image-reflector-controller,image-automation-controller \
   --arch=arm64 --version=latest \
   --export  > "./cluster/flux-system/gotk-components.yaml"
