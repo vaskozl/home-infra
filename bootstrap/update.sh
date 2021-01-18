9b200e3cb4875761f105aafb74a1add193525998
Vasil Zlatanov <v@skozl.com>
2021-01-18 09:11:01 +0000

    Remove flux images controller and netpol

--- bootstrap/update.sh
+++ bootstrap/update.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 flux install \
   --components=source-controller,kustomize-controller,helm-controller,notification-controller \
-  --components-extra=image-reflector-controller,image-automation-controller \
+  --network-policy false
   --arch=arm64 --version=latest \
   --export  > "./cluster/flux-system/gotk-components.yaml"
