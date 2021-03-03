8d1de0c67467d75a53c391519085a1d5a334b068
Vasil Zlatanov <v@skozl.com>
2021-03-03 20:50:07 +0000

    Update images

--- scripts/update-images.sh
+++ scripts/update-images.sh
@@ -1,6 +1,6 @@
 grep -lRP 'image:' cluster | while read file; do \
   mv $file $file.bak
-  perl -M'Sort::Versions' -Mojo -ne '
+  perl -M'Sort::Versions' -MJSON -M'Data::Dump qw(dump)' -ne '
   our ($image, $tag);
   my $line = $_;
   if (/^\s+(image|repository):\s*(\S*?)($|:)(.*)/ and $2 or
@@ -10,19 +10,13 @@ grep -lRP 'image:' cluster | while read file; do \
     } else {
       ($image, $tag) = ($2, $4) if $2;
     }
-    if ($image and $tag and $image !~ /(\.io)/){
+    if ($image and $tag){
       $image =~ s/["]//g;
       $tag =~ s/["]//g;
       my $tag_regex = quotemeta($tag);
       $tag_regex =~ s/\d+/\\d\+/g;
       sub rd { $_ = shift; s/[^\.\d]//g; $_ }
-      my @tags;
-      if ($image =~ s!r\.sko\.ai/!!) {
-        @tags = @{g("https://r.sko.ai/v2/$image/tags/list")->json->{tags}};
-      } else {
-        $image = "library/$image" unless $image =~ m!/!;
-        @tags = map { $_->{name} } @{g("https://hub.docker.com/v2/repositories/$image/tags/?page=1&page_size=1000")->json->{results}};
-      }
+      my @tags = @{decode_json(qx{skopeo list-tags docker://$image})->{Tags}};
       my @versions = sort { versioncmp(rd($a),rd($b)) } grep { /^$tag_regex$/ } @tags;
       if (@versions) {
         my $newest = pop @versions;
