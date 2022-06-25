b83dba785b7c1afcd91628c50049d66cf4f67ca2
Vasil Zlatanov <v@skozl.com>
2022-06-25 11:28:49 +0100

    Delete sops fields before kubeconforming

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -60,6 +60,7 @@ find . -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' fil
   do
     echo "INFO - Validating kustomization ${file/%$kustomize_config}"
     kustomize build "${file/%$kustomize_config}" "${kustomize_flags[@]}" | \
+      yq "del(.sops)" | \
       kubeconform "${kubeconform_config[@]}"
     if [[ ${PIPESTATUS[0]} != 0 ]]; then
       exit 1
