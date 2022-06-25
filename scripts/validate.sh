55c9b0181936c733dcc08e83243542833e91e9fe
Vasil Zlatanov <v@skozl.com>
2022-06-25 11:38:33 +0100

    Specify stdin for yq

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -45,7 +45,7 @@ kubeconform_config=("-strict" "-ignore-missing-schemas" "-schema-location" "defa
 echo "INFO - Validating cluster"
 find ./cluster -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file;
   do
-    cat "${file}" | yq e "del(.sops)" | kubeconform "${kubeconform_config[@]}"
+    yq e "del(.sops)" "${file}" | kubeconform "${kubeconform_config[@]}"
     if [[ ${PIPESTATUS[0]} != 0 ]]; then
       exit 1
     fi
@@ -60,7 +60,7 @@ find . -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' fil
   do
     echo "INFO - Validating kustomization ${file/%$kustomize_config}"
     kustomize build "${file/%$kustomize_config}" "${kustomize_flags[@]}" | \
-      yq e "del(.sops)" | \
+      yq e "del(.sops)" - | \
       kubeconform "${kubeconform_config[@]}"
     if [[ ${PIPESTATUS[0]} != 0 ]]; then
       exit 1
