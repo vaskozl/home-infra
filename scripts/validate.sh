d53b9df2018f4a8fead076aa4ba232c32f0cbdd6
Vasil Zlatanov <v@skozl.com>
2022-06-25 11:49:54 +0100

    Only validate top level so patches don't fail

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -43,26 +43,11 @@ done
 kubeconform_config=("-strict" "-ignore-missing-schemas" "-schema-location" "default" "-schema-location" "/tmp/flux-crd-schemas" "-verbose")
 
 echo "INFO - Validating cluster"
-find ./cluster -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file;
-  do
-    yq e "del(.sops)" "${file}" | kubeconform "${kubeconform_config[@]}"
-    if [[ ${PIPESTATUS[0]} != 0 ]]; then
-      exit 1
-    fi
-done
-
 # mirror kustomize-controller build options
 kustomize_flags=("--load-restrictor=LoadRestrictionsNone")
-kustomize_config="kustomization.yaml"
 
-echo "INFO - Validating kustomize overlays"
-find . -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file;
-  do
-    echo "INFO - Validating kustomization ${file/%$kustomize_config}"
-    kustomize build "${file/%$kustomize_config}" "${kustomize_flags[@]}" | \
-      yq e "del(.sops)" - | \
-      kubeconform "${kubeconform_config[@]}"
-    if [[ ${PIPESTATUS[0]} != 0 ]]; then
-      exit 1
-    fi
-done
+echo "INFO - Validating kustomization cluster/kustomization.yaml"
+kustomize build cluster/kustomization.yaml "${kustomize_flags[@]}" | \
+  yq e "del(.sops)" - | \
+  kubeconform "${kubeconform_config[@]}"
+
