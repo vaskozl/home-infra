dcbc16360ebdffb04094b536aca5dc2108ebf06d
Vasil Zlatanov <v@skozl.com>
2022-09-24 15:04:29 +0100

    Remove now redundant bootstrap dir

--- bootstrap/bootstrap.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-flux bootstrap github \
-  --components=source-controller,kustomize-controller,helm-controller,notification-controller \
-  --components-extra=image-reflector-controller,image-automation-controller \
-  --arch=arm64 --version=latest \
-  --owner='vaskozl' \
-  --repository='home-infra' \
-  --path='./cluster'
-  --branch='main' --personal --verbose
