435667a7675286e2a97ae039eb762417bbd431ca
Vasil Zlatanov <v@skozl.com>
2024-05-16 20:55:43 +0100

    Fix haproxy scrape

--- cluster/mail/config/rspamd.local.lua
+++ cluster/mail/config/rspamd.local.lua
@@ -1,8 +1,10 @@
 local reconf = config['regexp']
 
 reconf['TO_WRONG_NAME'] = {
-  re = [[To=/(\w+) <\1@/]],
-  score = 12.0,
+  -- match:
+  -- To: foo <foo@example.com>;
+  re = [[To=/^\s*(\w+)\s+<\1@/i{header}]],
+  score = 7.5,
   mime_only = true,
   description = 'The name matches the local-part of the email address',
   group = 'headers'
