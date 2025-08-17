6f6e2080401f3b1115100b0fabfbbbadc776801c
Vasil Zlatanov <v@skozl.com>
2025-08-17 16:03:01 +0100

    Use plain sh in validate.sh

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -1,21 +1,28 @@
 #!/bin/sh
-set -o errexit
+set -e
 
-find . -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file;
-  do
+# validate each yaml file with yq
+find . -type f -name '*.yaml' -print0 | while IFS= read -r -d '' file
+do
     echo "INFO - Validating $file"
-    yq e 'true' "$file" > /dev/null
+    yq e 'true' "$file" >/dev/null
 done
 
 CRD_CATALOG='https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/{{.Group}}/{{.ResourceKind}}_{{.ResourceAPIVersion}}.json'
 
-kubeconform_config=("-strict" "-ignore-missing-schemas" "-schema-location" "default" "-schema-location" "$CRD_CATALOG" "-verbose")
+# since sh has no arrays, just use quoted strings
+KUBECONFORM_CONFIG="-strict -ignore-missing-schemas -schema-location default -schema-location $CRD_CATALOG -verbose"
 
 echo "INFO - Validating cluster"
-# mirror kustomize-controller build options
-kustomize_flags=("--load-restrictor=LoadRestrictionsNone")
+
+# same for kustomize flags
+KUSTOMIZE_FLAGS="--load-restrictor=LoadRestrictionsNone"
 
 echo "INFO - Validating kustomization cluster/kustomization.yaml"
-kustomize build cluster/ "${kustomize_flags[@]}" | \
-  yq e "del(.sops)" - | \
-  kubeconform "${kubeconform_config[@]}" || true
+# run pipeline, ignore kubeconform exit code
+if ! kustomize build cluster/ $KUSTOMIZE_FLAGS | \
+     yq e 'del(.sops)' - | \
+     xargs kubeconform $KUBECONFORM_CONFIG
+then
+    true
+fi
