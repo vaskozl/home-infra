54e2640d588f11bf59ea66018227b618535f692d
Vasil Zlatanov <v@skozl.com>
2026-09-07 21:03:03 +0100

    Improve redis readiness probe and memory settings

--- cluster/common/redis/scripts/redis.sh
+++ cluster/common/redis/scripts/redis.sh
@@ -2,7 +2,8 @@
 set -e
 # shellcheck source=scripts/lib.sh
 . /usr/local/bin/lib.sh
-MASTER="$(resolve_master)"
+master="$(resolve_master)"
+maxmemory=$(( ${MEMORY_LIMIT_BYTES:-$((384*1024*1024))} * 3 / 4 ))
 cat > /run/redis/redis.conf <<EOF
 port 6379
 protected-mode no
@@ -12,6 +13,10 @@ save ""
 min-replicas-to-write 1
 min-replicas-max-lag 10
 replica-announce-ip $ME
+maxmemory $maxmemory
+maxmemory-policy ${MAXMEMORY_POLICY:-noeviction}
+client-output-buffer-limit replica $(( maxmemory / 10 )) $(( maxmemory / 20 )) 60
+repl-backlog-size $(( maxmemory / 100 ))
 EOF
-[ "$MASTER" = "$ME" ] || echo "replicaof $MASTER 6379" >> /run/redis/redis.conf
+[ "$master" = "$ME" ] || echo "replicaof $master 6379" >> /run/redis/redis.conf
 exec redis-server /run/redis/redis.conf
